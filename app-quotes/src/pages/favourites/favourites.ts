87baf3d41829b8883aa2b4af3ddf4af904d150e5
gslyle <gareth_lyle@yahoo.com>
2018-01-27 20:55:22 +0200

    adding and presenting modal

--- app-quotes/src/pages/favourites/favourites.ts
+++ app-quotes/src/pages/favourites/favourites.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Quote } from '../../data/quote.interface';
 import { QuotesService } from '../../services/quotes';
+import { ModalController } from 'ionic-angular';
+import { QuotePage } from '../quote/quote';
 
 @Component({
   selector: 'page-favourites',
@@ -9,9 +11,14 @@ import { QuotesService } from '../../services/quotes';
 export class FavouritesPage {
   quotes: Quote[];
 
-  constructor (private quotesService: QuotesService){}
+  constructor (private quotesService: QuotesService, private modalCtrl:ModalController){}
 
   ionViewWillEnter(){
     this.quotes = this.quotesService.getFavouriteQuotes();
+  }
+
+  onViewQuote(quote: Quote){
+    const modal = this.modalCtrl.create(QuotePage);
+    modal.present();
   }
 }
